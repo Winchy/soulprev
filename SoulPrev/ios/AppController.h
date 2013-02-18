c2781abe0958c89c02e596f001a2a43cda9441d3
Winchy <winchyyi@gmail.com>
2013-02-18 21:53:43 +0800

    Initial Commit

---
+++ SoulPrev/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SoulPrevAppController.h
+//  SoulPrev
+//
+//  Created by 易 文植 on 2/18/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SoulPrev/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SoulPrevAppController.h
+//  SoulPrev
+//
+//  Created by 易 文植 on 2/18/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
